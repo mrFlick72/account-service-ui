8c10a1105a92aeffc43e120490be21ad671308e4
mrflick72 <valerio.vaudi@gmail.com>
2021-05-20 22:31:51 +0200

    WIP first commit

--- src/test/java/it/valeriovaudi/onlyoneportal/accountservice/support/WithMockOidcUser.java
+++ src/test/java/it/valeriovaudi/onlyoneportal/accountservice/support/WithMockOidcUser.java
@@ -1,20 +1,9 @@
 package it.valeriovaudi.onlyoneportal.accountservice.support;
 
 
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.oidc.OidcIdToken;
-import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.test.context.support.WithSecurityContext;
-import org.springframework.security.test.context.support.WithSecurityContextFactory;
 
 import java.lang.annotation.*;
-import java.time.Instant;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Target({ElementType.METHOD, ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
