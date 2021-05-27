d3604ca0a4884040b21ecb38ee21d4569d9ba591
mrflick72 <valerio.vaudi@gmail.com>
2021-05-27 23:23:52 +0200

    configuration

--- src/main/java/it/valeriovaudi/onlyoneportal/accountservice/config/UseCaseConfig.java
+++ src/main/java/it/valeriovaudi/onlyoneportal/accountservice/config/UseCaseConfig.java
@@ -1,6 +1,5 @@
 package it.valeriovaudi.onlyoneportal.accountservice.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import it.valeriovaudi.onlyoneportal.accountservice.domain.UpdateAccount;
 import it.valeriovaudi.onlyoneportal.accountservice.domain.repository.AccountRepository;
 import org.springframework.context.annotation.Bean;
