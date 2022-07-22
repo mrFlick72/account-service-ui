0ac90c27bc1cb60e85b0335f8a1e76b63912b8c7
mrflick72 <valerio.vaudi@gmail.com>
2022-07-23 00:23:02 +0200

    content with material ui except of datepicker

--- src/main/frontend/app/AccountDetailsPage.js
+++ src/main/frontend/app/AccountDetailsPage.js
@@ -8,133 +8,13 @@ import FormInputTextField from "./component/form/FormInputTextField";
 import Separator from "./component/form/Separator";
 // import {useTheme} from '@mui/material/styles';
 import CheckIcon from '@mui/icons-material/Check';
-import FormDatePicker from "./component/form/FormDatePicker";
+import FormDatePicker, {DateFormatPattern} from "./component/form/FormDatePicker";
+import moment from "moment";
 
 const links = {
     logOut: "/account/oidc_logout.html",
     home: "/family-budget/index"
 };
-/*
-
-class AccountDetailsPage extends React.Component {
-
-    constructor(props) {
-        super(props);
-        state = {
-            messageRegistry: [],
-            firstName: "",
-            lastName: "",
-            phone: "",
-            birthDate: "",
-            mail: ""
-        };
-
-        save = save.bind(this);
-        accountRepository = new AccountRepository();
-        messageRepository = new MessageRepository();
-
-        firstNameInputRef = React.createRef();
-        lastNameInputRef = React.createRef();
-        phoneInputRef = React.createRef();
-        birthDateInputRef = React.createRef();
-        mailInputRef = React.createRef();
-    }
-
-    save() {
-        let account = {
-            firstName: firstNameInputRef.current.value,
-            lastName: lastNameInputRef.current.value,
-            phone: phoneInputRef.current.value,
-            birthDate: birthDateInputRef.current.value
-        };
-
-        accountRepository.save(account);
-    }
-
-    componentDidMount() {
-        accountRepository.getAccountData()
-            .then((data) => {
-                firstNameInputRef.current.value = data.firstName;
-                lastNameInputRef.current.value = data.lastName;
-                phoneInputRef.current.value = data.phone;
-                birthDateInputRef.current.value = data.birthDate;
-                mailInputRef.current.value = data.mail;
-            })
-
-        getMessages()
-            .then((data) => {
-                setState({messageRegistry: data})
-            })
-    }
-
-    render() {
-        // const theme = useTheme();
-
-        return (
-            <Paper variant="outlined">
-                <Menu messages={{
-                          title: getMessagesFor(messageRegistry, "common.title"),
-                          logOutLabel: getMessagesFor(messageRegistry, "logout.label")
-                      }} links={links}></Menu>
-
-                <div className="container">
-                    <div className="content">
-                        <div className="form-group">
-                            <label
-                                htmlFor="firstName">{getMessagesFor(messageRegistry, "form.firstName.label")} </label>
-                            <input type="text" className="form-control" ref={firstNameInputRef}
-                                   id="firstName"
-                                   placeholder={getMessagesFor(messageRegistry, "form.firstName.placeholder")}/>
-                        </div>
-                        <div className="form-group">
-                            <label
-                                htmlFor="lastName">{getMessagesFor(messageRegistry, "form.lastName.label")}</label>
-                            <input type="text" className="form-control" ref={lastNameInputRef}
-                                   id="lastName"
-                                   placeholder={getMessagesFor(messageRegistry, "form.lastName.placeholder")}/>
-                        </div>
-
-                        <div className="form-group">
-                            <label
-                                htmlFor="birthDate">{getMessagesFor(messageRegistry, "form.birthDate.label")}</label>
-                            {/!*      <DatePicker inputProps={{id: "birthDate", ref: birthDateInputRef}}
-                                        input={true}
-                                        closeOnSelect={true}
-                                        dateFormat="DD/MM/YYYY"
-                                        isValidDate={() => true}
-                                        timeFormat={false}/>*!/}
-                        </div>
-
-                        <div className="form-group">
-                            <label
-                                htmlFor="phone">{getMessagesFor(messageRegistry, "form.phone.label")}</label>
-                            <input type="text" className="form-control" ref={phoneInputRef}
-                                   id="phone"
-                                   placeholder={getMessagesFor(messageRegistry, "form.phone.placeholder")}/>
-                        </div>
-
-                        <div className="form-group">
-                            <label
-                                htmlFor="mail">{getMessagesFor(messageRegistry, "form.mail.label")}</label>
-                            <input type="text" className="form-control" ref={mailInputRef}
-                                   id="mail"
-                                   placeholder={getMessagesFor(messageRegistry, "form.mail.placeholder")}
-                                   readOnly="readonly"/>
-                        </div>
-
-                        <div className="form-group">
-
-                            <button type="submit" className="btn btn-success" onClick={save}>
-                                <i className="fas fa-check fa-lg"></i> {getMessagesFor(messageRegistry, "form.save.value")}
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </Paper>
-        )
-    }
-}
-*/
 
 const AccountDetailsPage = () => {
 
@@ -186,13 +66,13 @@ const AccountDetailsPage = () => {
                                 }}
                                 value={lastName || ""}/>
 
-            {/* <FormDatePicker
-                value={birthDate}
+            <FormDatePicker
+                value={moment(birthDate, DateFormatPattern)}
                 onClickHandler={(value) => {
-                    setBirthDate(value.target.value)
+                    setBirthDate(value.format(DateFormatPattern))
                 }}
                 label={getMessagesFor(messageRegistry, "form.birthDate.label")}/>
-            */}
+
             <FormInputTextField id="phone"
                                 label={getMessagesFor(messageRegistry, "form.phone.label")}
                                 required={true}
--- src/main/frontend/app/component/form/FormDatePicker.js
+++ src/main/frontend/app/component/form/FormDatePicker.js
@@ -1,17 +1,24 @@
 import React from "react";
-import {Button, Grid, TextField} from "@mui/material";
-import DesktopDatePicker from "@mui/x-date-pickers"
+import {Grid, TextField} from "@mui/material";
+import {DesktopDatePicker} from "@mui/x-date-pickers"
+import {AdapterMoment} from '@mui/x-date-pickers/AdapterMoment';
+import {LocalizationProvider} from '@mui/x-date-pickers/LocalizationProvider';
 
 export default function FormDatePicker({label, value, onClickHandler}) {
     return <Grid container alignItems="flex-end" style={{marginTop: '10px'}}>
         <Grid item md={true} sm={true} xs={true} justify="flex-end">
-            <DesktopDatePicker
-                label={label}
-                inputFormat="dd/MM/yyyy"
-                value={value}
-                onClick={onClickHandler || {}}
-                renderInput={(params) => <TextField {...params} />}
-            />
+            <LocalizationProvider dateAdapter={AdapterMoment}>
+                <DesktopDatePicker
+                    label={label}
+                    inputFormat={DateFormatPattern}
+                    value={value}
+                    onChange={onClickHandler || {}}
+                    renderInput={(params) => <TextField {...params} fullWidth />}
+                />
+            </LocalizationProvider>
         </Grid>
     </Grid>
+
 }
+
+export const DateFormatPattern = 'DD/MM/YYYY'
